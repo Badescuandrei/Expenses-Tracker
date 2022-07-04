c77eec01b4e305c6f26035e7bb68ebfd77810729
Badescuandrei <40524578+Badescuandrei@users.noreply.github.com>
2022-07-04 18:32:51 +0200

    Small changes

--- src/components/Filter/ExpenseFilter.js
+++ src/components/Filter/ExpenseFilter.js
@@ -5,21 +5,21 @@ const ExpenseFilter = (props) => {
   // const [selectedDate, setSelectedDate] = useState('');
 
   const datePickerHandler = (event) => {
-   props.onSelectFilter(event.target.value);
-  }
-
-    return (
-      <div className="expenses-filter">
-        <div className="expenses-filter__control">
-          <label>Filter by year</label>
-          <select value={props.selected} onChange={datePickerHandler}>
-            <option value="2022">2022</option>
-            <option value="2021">2021</option>
-            <option value="2020">2020</option>
-            <option value="2019">2019</option>
-          </select>
-        </div>
-      </div>
-    );
+    props.onSelectFilter(event.target.value);
   };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={datePickerHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
 export default ExpenseFilter;
